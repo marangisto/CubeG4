da130b3dbb30a74065aa61459442d95f53ccd450
Bengt Marten Agren <maraingist@me.com>
2019-08-04 21:28:50 +0800

    Closer to working ADC driver.

--- examples/ADC/Main.cpp
+++ examples/ADC/Main.cpp
@@ -11,6 +11,8 @@ using namespace hal::adc;
 
 typedef usart_t<2, PA2, PA3> serial;
 typedef output_t<PA5> ld4;
+typedef output_t<PA10> d2;
+typedef analog_t<PA0> ain;
 typedef adc_t adc;
 
 void loop();
@@ -22,10 +24,13 @@ template<> void handler<interrupt::USART2>()
 }
 
 extern "C" int ll_main();
+extern "C" void minimal_init();
 
 int main()
 {
     ld4::setup();
+    ain::setup();
+    d2::setup();
     serial::setup<230400>();
     hal::nvic<interrupt::USART2>::enable();
     interrupt::enable();
@@ -33,7 +38,8 @@ int main()
     stdio_t::bind<serial>();
     printf("Welcome to the STM32G431!\n");
 
-    ll_main();
+    adc::setup();
+    minimal_init();
 
     for (;;)
         loop();
@@ -46,8 +52,10 @@ void loop()
     printf("> \n");
     if (fgets(buf, sizeof(buf), stdin))
     {
+        d2::toggle();
         adc::start_conversion();
         while (!adc::end_of_conversion());
+        d2::toggle();
         uint16_t y = adc::read();
 
         printf("adc = %d\n", y);
